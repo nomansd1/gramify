658ee8fb567a93c1ac613de6ff532b1a72649a6f
Syed Noman Ali <noman.sd114@gmail.com>
2025-09-26 13:04:50 -0700

    added get specific controller

--- src/controllers/v1/story.controllers.js
+++ src/controllers/v1/story.controllers.js
@@ -36,11 +36,10 @@ export const uploadStory = asyncHandler(async (req, res) => {
   }
 
   console.log("cloud media: ", cloudMedia);
-  
 
   if (cloudMedia.length > 0) {
     try {
-      const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000); 
+      const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
       const story = await prisma.story.create({
         data: {
           userId: id,
@@ -48,7 +47,7 @@ export const uploadStory = asyncHandler(async (req, res) => {
           url: cloudMedia[0].url,
           type: cloudMedia[0].type,
           fileId: cloudMedia[0].fileId,
-          expiresAt
+          expiresAt,
         },
         select: {
           id: true,
@@ -70,7 +69,7 @@ export const uploadStory = asyncHandler(async (req, res) => {
       });
 
       const io = getSocket();
-      io.emit("story:created", story); 
+      io.emit("story:created", story);
 
       return res
         .status(StatusCodes.CREATED)
@@ -91,4 +90,83 @@ export const uploadStory = asyncHandler(async (req, res) => {
       throw error;
     }
   }
+});
+
+export const deleteStory = asyncHandler(async (req, res) => {
+  const { id: storyId } = req.params;
+  const userId = req.user.id;
+
+  const story = await prisma.story.findUnique({
+    where: {
+      id: storyId,
+      userId,
+    },
+    select: {
+      id: true,
+      fileId: true,
+      userId: trues,
+    },
+  });
+
+  if (!story) {
+    throw new ApiError(StatusCodes.NOT_FOUND, "Story not found");
+  }
+
+  if (story.userId !== userId) {
+    throw new ApiError(
+      StatusCodes.FORBIDDEN,
+      "You don't have permission to delete it"
+    );
+    s;
+  }
+
+  await prisma.story.delete({
+    where: {
+      id: storyId,
+      userId,
+    },
+  });
+
+  return res
+    .status(StatusCodes.OK)
+    .json(new ApiResponse(StatusCodes.OK, "Story has deleted successfully"));
+});
+
+export const getFollowingUsersStories = asyncHandler(async (req, res) => {
+
+});
+
+export const getSpecificStory = asyncHandler(async (req, res) => {
+  const { id: storyId } = req.params;
+
+  const story = await prisma.story.findUnique({
+    where: {
+      id: storyId,
+    },
+    select: {
+      id: true,
+      caption: true,
+      url: true,
+      type: true,
+      fileId: true,
+      createdAt: true,
+      expiresAt: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          username: true,
+          profilePicture: true,
+        },
+      },
+    },
+  });
+
+  if (!story) {
+    throw new ApiError(StatusCodes.NOT_FOUND, "Story not found");
+  }
+
+  return res
+    .status(StatusCodes.OK)
+    .json(new ApiResponse(StatusCodes.OK, "Story fetched successfully", story));
 });
